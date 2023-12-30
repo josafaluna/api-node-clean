ccf26fd498699f61cd31d0ab481b7df77339ea3a
Josafá Luna <josafaluna@gmail.com>
2023-12-30 11:19:12 -0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8'
+}
+
+module.exports = config
